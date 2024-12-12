51ee9a982f4ca777308cfbb0af78c600436c9ef2
Patrick Kingston <pkingston@bowdoin.edu>
2024-12-12 13:54:35 -0500

    Started working on brush and basic datastructures

--- README.md
+++ README.md
@@ -4,15 +4,15 @@
 
 Main goals:
 
-1. [ ] Create dummy palette
-1. [ ] Create canvas grid
-1. [ ] Create active_cells grid
-1. [ ] Create dummy transition table
+1. [x] Create dummy palette
+1. [x] Create cells grid
+1. [x] Create dummy transition table
 1. [ ] For circle and square brush:
-   - Make it resizable
-   - Make it set the required cells to active
+   - [ ] Let the user select a color from the palette
+   - [ ] Make it set the required cells to active
      - Note: There will need to be different "colors" as "strokes" representing different active states. Furthermore, they will need to overwrite the ones under them.
-   - Make it set the center pixel to the selected palette color
+   - [ ] Make it set the center pixel to the selected palette color
+   - [ ] Make it resizable
 1. [ ] Quantize image into set colors
 1. [ ] Ingest image into markov table
 1. [ ] Use set colors as palette
--- sketch.js
+++ sketch.js
@@ -1,20 +1,82 @@
 
+function Cell(isseed, active, color, xpos, ypos) {
+  this.isseed = isseed; //bool
+  this.active = active; //int
+  this.color = color; //int index <8
+  this.xpos = xpos; //int <600
+  this.ypos = ypos; //int <600
+}
 
+let canvas_size, palette, transitions, background_color, cells, stroke, active_strokes;
 
 function setup() {
-  // Create a canvas 400x400 pixels
-  createCanvas(600, 600);
+  // Constants
+  canvas_size = 600;
 
-  // Set the background color
-  background(220); // Light gray
+  // The palette for the final project will have 8 items, indexed 0-7 each is a color's hex value
+  // TODO: base on ingesting image
+  palette = [0x000, 0xfff]//, 0xf00, 0x0f0, 
+           //0x00f, 0xff0, 0xf0f, 0x0ff];
+  // Basic transition table that will flip between black and white
+  // TODO: base on ingesting image
+  transitions = {0: {0: 0.2,
+                     1: 0.8,},
+                 1: {0: 0.8,
+                     1: 0.2}};
+  // TODO: Make changable
+  background_color = 0x000;
+
+  // Initialize array of cells
+  cells = [];
+  for (var i = 0; i < canvas_size; i++) {
+    cells.push([]);
+    for (var j = 0; j < canvas_size; j++) {
+      cells[i].push(new Cell(false, 0, background_color, j, i));
+      // Cells start out NOT as seeds, inactive, default color, with the right position
+    }
+  }
+
+  // Initialize active strokes (no strokes still active)
+  stroke = 1;
+  active_strokes = [];
+
+  // Create a canvas 400x400 pixels
+  createCanvas(canvas_size, canvas_size);
+
+  // Make pixels accessible
+  loadPixels();
+
+  // Draw the background color
+  for (var i = 0; i < cells.length; i++) {
+    for (var j = 0; j < cells[0].length; j++) {
+      cell = cells[i][j];
+      set(cell.xpos, cell.ypos, cell.color);
+    }
+  }
+  updatePixels();
 }
 
 // Draw function, loops continuously after setup
 function draw() {
-  // Set the fill color
-  fill(0); // Black
 
-  // Draw a circle at the mouse position
-  ellipse(mouseX, mouseY, 50, 50);
+  //////////////////
+  // HANDLE INPUT //
+  //////////////////
+  if (mousePressed) {
+    // Initialize a stroke
+    active_strokes.push(stroke);
+    stroke += 1;
+  }
+  ///////////////
+  // DRAW STEP //
+  ///////////////
+  // Draw the cells every frame
+  for (var i = 0; i < cells.length; i++) {
+    for (var j = 0; j < cells[0].length; j++) {
+      cell = cells[i][j];
+      set(cell.xpos, cell.ypos, cell.color);
+    }
+  }
+  updatePixels();
 }
 
